cb1762a777c7609e48ccac1547abdf70b01a2ec6
Rizqi Setiaji <rizqisetiaji9@gmail.com>
2024-08-19 16:53:01 +0700

    Create Navbar component

---
+++ src/components/navigation/Navbar.tsx
@@ -0,0 +1,62 @@
+import Link from "next/link"
+import Icons from "../global/icons"
+import { buttonVariants } from "../ui/button"
+
+const Navbar = () => {
+	const user = false
+
+	return (
+		<header className="px-4 h-16 sticky top-0 inset-x-0 w-full bg-background/40 backdrop-blur-lg border-b border-border z-50">
+			<div className="flex items-center justify-between h-full mx-auto md:max-w-screen-xl">
+				<div className="flex items-start">
+					<Link href="/" className="flex items-center gap-2">
+						<Icons.logo className="w-8 h-8" />
+						<span className="text-lg font-medium">Astra</span>
+					</Link>
+				</div>
+
+				<nav className="hidden md:flex absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2">
+					<ul className="flex items-center gap-8">
+						<li className="hover:text-foreground/80 text-sm">
+							<Link href="#">Pricing</Link>
+						</li>
+						<li className="hover:text-foreground/80 text-sm">
+							<Link href="#">About</Link>
+						</li>
+						<li className="hover:text-foreground/80 text-sm">
+							<Link href="#">Features</Link>
+						</li>
+						<li className="hover:text-foreground/80 text-sm">
+							<Link href="#">Blog</Link>
+						</li>
+					</ul>
+				</nav>
+
+				<div className="flex items-center gap-4">
+					{ user ? (
+						'User Button'
+					) : (
+						<>
+							<Link href="/sign-in" className={buttonVariants({
+									size: 'sm',
+									variant: 'ghost'
+								})
+							}>
+								Login
+							</Link>
+							<Link href="/sign-up" className={buttonVariants({
+									size: 'sm',
+									className: 'hidden md:flex'
+								})
+							}>
+								Start free trial
+							</Link>
+						</>
+					) }
+				</div>
+			</div>
+		</header>
+	)
+}
+
+export default Navbar
