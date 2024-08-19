e77eefa28bd8a64256a730c42b750f86bdf0b612
Rizqi Setiaji <rizqisetiaji9@gmail.com>
2024-08-19 16:50:19 +0700

    set indentation

--- src/app/(marketing)/layout.tsx
+++ src/app/(marketing)/layout.tsx
@@ -5,11 +5,11 @@ interface Props {
 }
 
 const MarketingLayout = ({ children }: Props) => {
-    return (
-        <div className="flex flex-col items-center w-full">
-            {children}
-        </div>
-    )
+	return (
+		<div className="flex flex-col items-center w-full">
+			{children}
+		</div>
+	)
 };
 
 export default MarketingLayout
