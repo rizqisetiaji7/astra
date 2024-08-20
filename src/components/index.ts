a6476456da099ba7fc8a39d49035ead116711c68
Rizqi Setiaji <rizqisetiaji9@gmail.com>
2024-08-21 01:44:54 +0700

    Import & Export Footer component

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,10 +3,12 @@ import Container from './global/Container'
 import Wrapper from './global/Wrapper'
 
 import Navbar from './navigation/Navbar'
+import Footer from './navigation/Footer'
 
 export {
     Icons,
     Container,
     Wrapper,
-    Navbar
+    Navbar,
+    Footer
 }
