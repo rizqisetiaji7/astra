5b20388d2a0fcd0967c5fb37275ec0c046c1f923
Rizqi Setiaji <rizqisetiaji9@gmail.com>
2024-08-20 13:40:08 +0700

    Add Pricing Section

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -1,11 +1,20 @@
-import { Container, Icons, Wrapper } from "@/components";
-import { BorderBeam } from "@/components/ui/border-beam";
-import { Button } from "@/components/ui/button";
-import SectionBadge from "@/components/ui/section-badge";
-import { features, perks } from "@/constants";
-import { ArrowRight, ChevronRight } from "lucide-react";
-import Image from "next/image";
-import Link from "next/link";
+import { Container, Icons, Wrapper } from "@/components"
+import { 
+	Card, 
+	CardContent, 
+	CardDescription, 
+	CardFooter, 
+	CardHeader, 
+	CardTitle 
+} from "@/components/ui/card"
+import { BorderBeam } from "@/components/ui/border-beam"
+import { Button } from "@/components/ui/button"
+import SectionBadge from "@/components/ui/section-badge"
+import { features, perks, pricingCards } from "@/constants"
+import { ArrowRight, ChevronRight, Zap } from "lucide-react"
+import Image from "next/image"
+import Link from "next/link"
+import { cn } from "@/lib/utils"
 
 const HomePage = () => {
 	return (
@@ -61,7 +70,7 @@ const HomePage = () => {
 			{/* How it works */}
 			<Wrapper className="flex flex-col items-center justify-center py-12 relative">
 				<Container>
-					<div className="max-w-md mx-auto text-start md:text-center">
+					<div className="max-w-md md:mx-auto text-start md:text-center">
 						<SectionBadge title="The Process" />
 						<h2 className="text-3xl lg:text-4xl font-semibold mt-6">
 							Three steps to build your dream website
@@ -93,7 +102,7 @@ const HomePage = () => {
 			{/* Features */}
 			<Wrapper className="flex flex-col items-center justify-center py-12 relative">
 				<Container>
-					<div className="max-w-md mx-auto text-start md:text-center">
+					<div className="max-w-md md:mx-auto text-start md:text-center">
 						<SectionBadge title="Features" />
 						<h2 className="text-3xl lg:text-4xl font-semibold mt-6">
 							Discover our powerful features
@@ -121,6 +130,63 @@ const HomePage = () => {
 								</div>
 							))}
 						</div>
+					</div>
+				</Container>
+			</Wrapper>
+
+			{/* Pricing */}
+			<Wrapper className="flex flex-col items-center justify-center py-12 relative">
+				<Container>
+					<div className="max-w-md md:mx-auto text-start md:text-center">
+						<SectionBadge title="Pricing" />
+						<h2 className="text-3xl lg:text-4xl semibold mt-6">
+							Unlock the right plan for your business
+						</h2>
+						<p className="text-muted-foreground mt-6">
+							Choose the best plan for your business and start building your dream website today
+						</p>
+					</div>
+				</Container>
+				<Container className="flex items-center justify-center">	
+					<div className="grid grid-cols-1 lg:grid-cols-3 gap-5 w-full md:gap-8 py-10 md:py-20 flex-wrap max-w-4xl">
+						{ pricingCards.map(card => (
+							<Card 
+								key={card.priceId}
+								className={cn(
+									'flex flex-col w-full border-neutral-700',
+									card.title === 'Unlimited Saas' && 'border-2 border-primary'
+								)}
+							>
+								<CardHeader className="border-2 border-border">
+									<span>{ card.title }</span>
+									<CardTitle className={cn(card.title !== 'Unlimited Saas' && 'text-muted-foreground')}>
+										{ card.price }
+									</CardTitle>
+									<CardDescription>
+										{ card.description }
+									</CardDescription>
+								</CardHeader>
+								<CardContent className="pt-6 space-y-3">
+									{ card.features.map(feature => (
+										<div key={feature} className="flex items-center gap-2">
+											<Zap className="h-4 w-4 fill-primary text-primary" />
+											<p>{ feature }</p>
+										</div>
+									)) }
+								</CardContent>
+								<CardFooter className="mt-auto w-full">
+									<Link 
+										href="#"
+										className={cn(
+											'w-full text-center text-primary-foreground bg-primary p-2 rounded-md text-sm font-medium',
+											card.title !== 'Unlimited Saas' && '!bg-foreground !text-background'
+										)}
+									>
+										{ card.buttonText }
+									</Link>
+								</CardFooter>
+							</Card>
+						)) }
 					</div>
 				</Container>
 			</Wrapper>
