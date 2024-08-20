f0a511a0df95ec23aeb9775976988ab7854e19da
Rizqi Setiaji <rizqisetiaji9@gmail.com>
2024-08-21 02:24:17 +0700

    Add auth route including sign-in & sign-up page

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,13 @@
+interface Props {
+   children: React.ReactNode
+}
+
+const AuthLayout = ({ children }:Props) => {
+   return (
+      <div className="flex items-center justify-center h-screen">
+         { children }
+      </div>
+   )
+}
+
+export default AuthLayout
